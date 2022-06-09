24dc4bdc90eeda94e1204ee2ee5af619d16a5537
srinaudo <s.rinaudo@contur.it>
2022-06-09 11:29:57 +0200

    Update

--- app.go
+++ app.go
@@ -11,7 +11,38 @@ func (p *Point) ScaleBy(factor float64) {
 	p.Y *= factor
 }
 
+
+/**
+ * In questo esempio il riferimento passato 
+ * può essere nil, in questo caso è best practice 
+ * specificare questa possibilità nell'implementazione ( if list == nil )
+ */
+type IntList struct {
+	Value int
+	Tail *IntList
+}
+
+func (list *IntList) Sum() int {
+	if list == nil { // qui: esplicitare che potrebbe arrivare nil
+		return 0
+	}
+	return list.Value + list.Tail.Sum()
+}
+
 func main() {
+	linkedListOfIntegersExample()
+	// pointExample()
+}
+
+func linkedListOfIntegersExample() {
+	l1 := IntList{1, nil}
+  l2 := IntList{3, &l1}
+
+  
+	fmt.Println(l2.Sum())
+}
+
+func pointExample() {
 	p := Point{1, 2}
 	p.ScaleBy(2)
 
